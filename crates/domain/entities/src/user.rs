7425ee0249a51fedbe07aef80d70632c0336a6c0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-12 07:51:16 +0300

    Add remaining user fields

--- crates/domain/entities/src/user.rs
+++ crates/domain/entities/src/user.rs
@@ -1,7 +1,16 @@
 use crate::entity::Entity;
-use domain_macros::{Entity, add_entity_fields};
+use domain_macros::{add_entity_fields, Entity};
+
+#[repr(u8)]
+pub enum UserState {
+    Active = 0,
+    Inactive = 1,
+    Suspended = 2,
+}
 
 #[derive(Entity)]
 #[add_entity_fields]
 pub struct User {
+    pub display_name: String,
+    pub state: UserState,
 }
