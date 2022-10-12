8f3f98bbb31a7e69db9e8471f5d630289c058dd9
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-13 02:19:10 +0300

    Set correct fields of `AccountRole`

--- crates/kernel/entities/src/entities/account_role.rs
+++ crates/kernel/entities/src/entities/account_role.rs
@@ -1,10 +1,12 @@
 use crate::traits::*;
 use domain_macros::*;
 
+use super::{Role, Account};
+
 #[derive(Entity)]
 #[entity]
 pub struct AccountRole {
-    pub friendly_name: Option<String>,
-    pub code: String,
+    pub account_id: <Account as Identifiable>::Key,
+    pub role_id: <Role as Identifiable>::Key,
     pub enabled: bool,
 }
