8bcb6d7883a6ccfd35c3f22a2449349e6bb46602
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-13 06:53:04 +0300

    Rewrite entity implementation macro interface

--- crates/kernel/proc_macros/src/lib.rs
+++ crates/kernel/proc_macros/src/lib.rs
@@ -1,33 +1,12 @@
 mod entity;
 
 use proc_macro::TokenStream;
-
-#[proc_macro_derive(Identifiable)]
-pub fn identifiable_fn(input: TokenStream) -> TokenStream {
-    entity::implement_entity_trait::<false, false>(input.into()).into()
-}
-
-#[proc_macro_derive(Entity)]
-pub fn entity_fn(input: TokenStream) -> TokenStream {
-    entity::implement_entity_trait::<true, false>(input.into()).into()
-}
-
-#[proc_macro_derive(MutableEntity)]
-pub fn mutable_entity_fn(input: TokenStream) -> TokenStream {
-    entity::implement_entity_trait::<true, true>(input.into()).into()
-}
+use syn::{AttributeArgs, DeriveInput};
 
 #[proc_macro_attribute]
-pub fn identifiable(_args: TokenStream, input: TokenStream) -> TokenStream {
-    entity::add_entity_fields::<false, false>(input.into()).into()
-}
+pub fn entity(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args = syn::parse_macro_input!(args as AttributeArgs);
+    let input = syn::parse_macro_input!(input as DeriveInput);
 
-#[proc_macro_attribute]
-pub fn entity(_args: TokenStream, input: TokenStream) -> TokenStream {
-    entity::add_entity_fields::<true, false>(input.into()).into()
-}
-
-#[proc_macro_attribute]
-pub fn mutable_entity(_args: TokenStream, input: TokenStream) -> TokenStream {
-    entity::add_entity_fields::<true, true>(input.into()).into()
+    entity::expand_entity(args, input).into()
 }
