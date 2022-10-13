bc05aff2dca61907004b64ca94b9dd3621779c21
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-13 07:27:21 +0300

    Add a getter method to `Repo` trait

--- crates/kernel/repositories/src/repo.rs
+++ crates/kernel/repositories/src/repo.rs
@@ -1,7 +1,11 @@
 use kernel_entities::traits::BasicEntity;
 
+use async_trait::async_trait;
+
+#[async_trait]
 pub trait Repo<Key, Entity>
 where
     Entity: BasicEntity<Key = Key>,
 {
+    async fn get(id: Key) -> anyhow::Result<Entity>;
 }
