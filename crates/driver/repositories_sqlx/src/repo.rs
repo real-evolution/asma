3901c2a33eaea2a6f03cc123b48ef8390d313399
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-15 06:29:25 +0300

    Extract database specification into a separate type

--- crates/driver/repositories_sqlx/src/repo.rs
+++ crates/driver/repositories_sqlx/src/repo.rs
@@ -3,16 +3,16 @@ use std::ops::{Deref, DerefMut};
 use kernel_entities::traits::BasicEntity;
 use kernel_repositories::Repo;
 
-pub type PostgresPool = sqlx::postgres::PgPool;
+pub type DbType = sqlx::postgres::Postgres;
 
 pub struct SqlxRepo {
-    pool: PostgresPool,
+    pool: sqlx::Pool<DbType>,
 }
 
 impl<E, K> Repo<E, K> for SqlxRepo where E: BasicEntity<Key = K> {}
 
 impl Deref for SqlxRepo {
-    type Target = PostgresPool;
+    type Target = sqlx::Pool<DbType>;
 
     fn deref(&self) -> &Self::Target {
         &self.pool
