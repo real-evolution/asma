84d507bcef95f1121bc025cc5310eba89c3962c7
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-16 22:22:02 +0300

    Use proper naming for web crates

--- crates/web/runner/src/main.rs
+++ crates/web/runner/src/main.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() {
     axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
-        .serve(host_rest_axum::router::make_router().into_make_service())
+        .serve(web_rest::router::make_router().into_make_service())
         .await
         .unwrap();
 }
