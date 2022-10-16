96b0609a6a836dcccbda003cdd291e96238e1bd0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-16 22:17:26 +0300

    Add a tonic runner server

--- crates/host/web/src/main.rs
+++ crates/host/web/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
+        .serve(host_rest_axum::router::make_router().into_make_service())
+        .await
+        .unwrap();
 }
