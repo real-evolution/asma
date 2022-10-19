0210d9ba7f5aa04229db27657f77c313aababbe3
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-19 22:59:10 +0300

    Add validation to `SqlxDataConfig`

--- crates/driver/repositories_sqlx/src/config.rs
+++ crates/driver/repositories_sqlx/src/config.rs
@@ -1,3 +1,5 @@
+use common_validation::validators::*;
+
 use serde::Deserialize;
 use validator::Validate;
 
@@ -5,9 +7,13 @@ pub const DATA_CONFIG_SECTION: &str = "data";
 
 #[derive(Debug, Deserialize, Validate)]
 pub struct SqlxDataConfig<'a> {
+    #[validate(custom = "supported_driver")]
     pub driver: &'a str,
+    #[validate(custom = "endpoint")]
     pub host: &'a str,
-    pub port: u16,
+    #[validate(range(min = 0, max = 0xFFFF))]
+    pub port: Option<u16>,
+    #[validate(custom = "basic_username")]
     pub username: &'a str,
     pub password: &'a str,
 }
