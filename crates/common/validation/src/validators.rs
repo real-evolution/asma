aebb1a50af2a3fa7bf60e04b194e27d7dafdb0f2
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-19 22:58:41 +0300

    Add commonly used validators

---
+++ crates/common/validation/src/validators.rs
@@ -0,0 +1,35 @@
+use validator::ValidationError;
+use validators::traits::ValidateString;
+
+use crate::parse::*;
+
+fn validate<V: ValidateString>(
+    code: &'static str,
+    value: &str,
+) -> Result<(), ValidationError> {
+    if V::parse_str(value).is_ok() {
+        return Ok(());
+    }
+
+    Err(ValidationError::new(code))
+}
+
+pub fn host(value: &str) -> Result<(), ValidationError> {
+    validate::<Host>("host", value)
+}
+
+pub fn endpoint(value: &str) -> Result<(), ValidationError> {
+    validate::<Endpoint>("endpoint", value)
+}
+
+pub fn supported_driver(value: &str) -> Result<(), ValidationError> {
+    validate::<SupportedDriver>("supported_driver", value)
+}
+
+pub fn basic_username(value: &str) -> Result<(), ValidationError> {
+    validate::<BasicUsername>("basic_username", value)
+}
+
+pub fn username(value: &str) -> Result<(), ValidationError> {
+    validate::<Username>("username", value)
+}
