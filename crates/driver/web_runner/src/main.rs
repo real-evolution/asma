f08c1d9212471c205053f07ea4763654911cd022
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-20 01:15:08 +0300

    Restructure project

--- crates/driver/web_runner/src/main.rs
+++ crates/driver/web_runner/src/main.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() {
     axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
-        .serve(web_rest::router::make_router().into_make_service())
+        .serve(driver_web_rest::router::make_router().into_make_service())
         .await
         .unwrap();
 }
