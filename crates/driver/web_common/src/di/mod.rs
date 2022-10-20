041754280be8bd37ac649b6c00f605b1f53604e1
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-20 05:37:18 +0300

    Export root module

--- crates/driver/web_common/src/di/mod.rs
+++ crates/driver/web_common/src/di/mod.rs
@@ -1,19 +1,17 @@
+pub extern crate shaku;
+
 mod root;
 mod services;
 
 use kernel_services::config::ConfigService;
-
-use root::*;
-use services::*;
-
+use shaku::HasComponent;
 use std::sync::Arc;
 
-use shaku::HasComponent;
-
+pub use root::RootModule;
 pub trait ServicesModule: HasComponent<dyn ConfigService> {}
 
 pub fn build_di() -> anyhow::Result<Arc<RootModule>> {
-    let services = services_module()?;
+    let services = services::services_module()?;
 
     Ok(Arc::new(RootModule::builder(services).build()))
 }
