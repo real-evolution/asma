13081a57d15f14476785942b35e501e40fbec7f0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-20 03:40:52 +0300

    Implement basic dependency injection

--- crates/driver/web_runner/src/main.rs
+++ crates/driver/web_runner/src/main.rs
@@ -1,7 +1,16 @@
+use axum::Extension;
+
+mod di;
+
 #[tokio::main]
 async fn main() {
+    let module = di::build_di_module();
+
+    let rest_app =
+        driver_web_rest::router::make_router().layer(Extension(module));
+
     axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
-        .serve(driver_web_rest::router::make_router().into_make_service())
+        .serve(rest_app.into_make_service())
         .await
         .unwrap();
 }
