5a5f077ce402b85d5e09558c066beabb8f77ea85
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-22 20:44:27 +0300

    Add auth api module

--- crates/driver/web_rest/src/api/mod.rs
+++ crates/driver/web_rest/src/api/mod.rs
@@ -1,7 +1,10 @@
+mod auth;
 mod diag;
 
 use axum::Router;
 
 pub fn api_routes() -> Router {
-    Router::new().nest("/diag", diag::diag_routes())
+    Router::new()
+        .nest("/diag", diag::routes())
+        .nest("/auth", auth::routes())
 }
