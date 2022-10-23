2a2ad9e00366a2725154f1704c1a09032df567cf
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-23 21:24:51 +0300

    Rewrite `ApiError` enum

--- crates/driver/web_rest/src/error.rs
+++ crates/driver/web_rest/src/error.rs
@@ -2,6 +2,8 @@ use axum::{
     extract::rejection::JsonRejection, http::StatusCode,
     response::IntoResponse, Json,
 };
+use kernel_repositories::error::RepoError;
+use kernel_services::error::AppError;
 use serde_json::json;
 use thiserror::Error;
 
@@ -18,31 +20,49 @@ pub enum ApiError {
     #[error(transparent)]
     ValidationError(#[from] validator::ValidationErrors),
 
-    #[error("invalid credentials")]
-    InvalidCredentials,
+    #[error(transparent)]
+    AppError(#[from] AppError),
+}
+
+fn status_tuple(status: StatusCode) -> (StatusCode, String) {
+    (status, status.to_string())
 }
 
 impl IntoResponse for ApiError {
     fn into_response(self) -> axum::response::Response {
         let (status, message) = match &self {
-            ApiError::InternalError(_) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, "internal server occured")
+            ApiError::InternalError(err) => {
+                error!("internal error: {err:?}");
+                status_tuple(StatusCode::INTERNAL_SERVER_ERROR)
             }
 
             ApiError::JsonError(_) => {
-                (StatusCode::BAD_REQUEST, "invalid request schema")
+                (StatusCode::BAD_REQUEST, "invalid request schema".into())
             }
 
             ApiError::ValidationError(_) => {
-                (StatusCode::BAD_REQUEST, "invalid request data")
+                (StatusCode::BAD_REQUEST, "invalid request data".into())
             }
 
-            ApiError::InvalidCredentials => {
-                (StatusCode::UNAUTHORIZED, "invalid credentails were used")
-            }
+            ApiError::AppError(err) => match err {
+                AppError::Config(_) | AppError::Unknown(_) => {
+                    status_tuple(StatusCode::INTERNAL_SERVER_ERROR)
+                }
+
+                AppError::Repo(err) => match err {
+                    RepoError::NotFound => {
+                        (StatusCode::NOT_FOUND, err.to_string())
+                    }
+                    _ => status_tuple(StatusCode::INTERNAL_SERVER_ERROR),
+                },
+
+                AppError::Auth(err) => {
+                    (StatusCode::UNAUTHORIZED, err.to_string())
+                }
+            },
         };
 
-        warn!("{status}: {self}");
+        warn!("{status}: {self:?}");
 
         (status, Json(json!({ "error": message }))).into_response()
     }
