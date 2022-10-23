4f6d14d0368b8cc5d949e061614481dd5e14cb1f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-23 21:48:07 +0300

    Update DI usage to include type

--- crates/driver/web_runner/src/launch.rs
+++ crates/driver/web_runner/src/launch.rs
@@ -4,12 +4,15 @@ use crate::config::*;
 
 use driver_web_common::di::*;
 use driver_web_rest::app::make_app;
+use kernel_services::config::ConfigService;
 
 use axum::Extension;
 use tower_http::trace::TraceLayer;
 
 pub async fn launch(di: Arc<DI>) -> anyhow::Result<()> {
-    let bind_addr = web::get_bind_address(di.resolve_ref())?;
+    let config_svc: &dyn ConfigService = di.resolve_ref();
+
+    let bind_addr = web::get_bind_address(config_svc)?;
     let server = axum::Server::try_bind(&bind_addr)?;
 
     info!("running server on: {bind_addr}");
--- crates/driver/web_runner/src/main.rs
+++ crates/driver/web_runner/src/main.rs
@@ -7,13 +7,15 @@ mod config;
 mod launch;
 
 use driver_web_common::di::{build_di, HasComponent};
+use kernel_services::config::ConfigService;
 use std::process::{ExitCode, Termination};
 
 #[tokio::main]
 async fn main() -> impl Termination {
     let di = build_di().expect("could not setup DI");
+    let config_svc: &dyn ConfigService = di.resolve_ref();
 
-    config::log::configure_logger_with(di.resolve_ref())
+    config::log::configure_logger_with(config_svc)
         .expect("could not setup logging");
 
     if let Err(err) = launch::launch(di).await {
