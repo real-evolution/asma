49a084730af4a617999d512796c99325d15aa908
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-23 03:59:25 +0300

    Fix patterns

--- crates/common/validation/src/patterns.rs
+++ crates/common/validation/src/patterns.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 lazy_static! {
     pub static ref RE_SUPPORTED_DRIVERS: Regex = Regex::new("^(postgres)$").unwrap();
     pub static ref RE_IDENTIFIER: Regex =
-        Regex::new(r#"[_a-zA-Z][_a-zA-Z0-9]{0,30}"#).unwrap();
+        Regex::new(r#"^[_a-zA-Z][_a-zA-Z0-9]{0,30}$"#).unwrap();
     pub static ref RE_USERNAME: Regex =
-        Regex::new(r#"^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){4,18}[a-zA-Z0-9]$"#).unwrap();
+        Regex::new(r#"^[_a-zA-Z][_a-zA-Z0-9]{2,30}$"#).unwrap();
 }
