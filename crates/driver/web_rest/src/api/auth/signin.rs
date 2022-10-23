19bb31c0960058185e28e46cd7aecebf3d8b3c61
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-23 21:24:24 +0300

    Add signin route related schema

--- crates/driver/web_rest/src/api/auth/signin.rs
+++ crates/driver/web_rest/src/api/auth/signin.rs
@@ -1,10 +1,19 @@
 use crate::{error::ApiResult, util::validated_json::ValidatedJson};
 use common_validation::username;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use utoipa::ToSchema;
 use validator::Validate;
 
+#[utoipa::path(
+    post,
+    path = "/api/auth/signin",
+    request_body = UserCredentials,
+    responses(
+        (status = 200, description = "Signed in successfully", body = TokenPair),
+        (status = 403, description = "Invalid credentials were received"),
+    ),
+)]
 pub async fn signin(
     ValidatedJson(form): ValidatedJson<UserCredentials>,
 ) -> ApiResult<String> {
@@ -15,6 +24,7 @@ pub async fn signin(
 }
 
 #[derive(ToSchema, Validate, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct UserCredentials {
     #[validate(custom = "username")]
     account_name: String,
@@ -22,4 +32,11 @@ pub struct UserCredentials {
     username: String,
     device_identifier: String,
     password: String,
+}
+
+#[derive(ToSchema, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TokenPair {
+    access_token: String,
+    refresh_token: String,
 }
--- crates/driver/web_rest/src/doc.rs
+++ crates/driver/web_rest/src/doc.rs
@@ -8,8 +8,13 @@ const OPENAPI_URL: &str = "/api-doc/openapi.json";
 
 #[derive(OpenApi)]
 #[openapi(
-    paths(api::diag::echo),
-    components(schemas()),
+    paths(
+        api::diag::echo,
+        api::auth::signin,
+    ),
+    components(
+        schemas(api::auth::UserCredentials, api::auth::TokenPair)
+    ),
     tags((
         name = "asma",
         description = "Advanced Social Media Aggregator"
