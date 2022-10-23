9b84038765de372ae04b278ba07220273cd2b5a8
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-23 23:04:43 +0300

    Remove `Repo` trait

--- crates/kernel/repositories/src/lib.rs
+++ crates/kernel/repositories/src/lib.rs
@@ -1,13 +1,11 @@
 pub mod error;
 
 mod accounts;
-mod repo;
 mod roles;
 mod sessions;
 mod users;
 
 pub use accounts::*;
-pub use repo::*;
 pub use roles::*;
 pub use sessions::*;
 pub use users::*;
--- crates/kernel/repositories/src/repo.rs
+++
@@ -1,7 +0,0 @@
-use kernel_entities::traits::BasicEntity;
-
-pub trait Repo<E, K>
-where
-    E: BasicEntity<Key = K>,
-{
-}
