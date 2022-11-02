3e577d715b03d5bd230e22f0d7c0c89a9aefa10c
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-02 20:23:15 +0300

    Update session entity to match DDL

--- crates/kernel/entities/src/entities/account.rs
+++ crates/kernel/entities/src/entities/account.rs
@@ -1,15 +1,15 @@
-use super::*;
-use crate::traits::*;
+use chrono::{DateTime, Utc};
 use kernel_proc_macros::*;
 
-use chrono::{DateTime, Utc};
+use super::*;
+use crate::traits::*;
 
 #[entity]
 #[derive(sqlx::FromRow)]
 pub struct Account {
     pub account_name: String,
     pub holder_name: Option<String>,
-    pub password: Option<String>,
+    pub password_hash: String,
     pub is_active: bool,
     pub valid_until: Option<DateTime<Utc>>,
     pub user_id: UserKey,
