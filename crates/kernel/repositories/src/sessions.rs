b0a421a0d59034ed943f18a4a995e9dc7c0a4086
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-02 20:23:01 +0300

    Complete sessions repository interface

--- crates/kernel/repositories/src/sessions.rs
+++ crates/kernel/repositories/src/sessions.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Duration, Utc};
 use kernel_entities::entities::*;
 use shaku::Interface;
 
@@ -20,10 +21,41 @@ pub trait SessionsRepo: Interface {
         device_identifier: &str,
     ) -> RepoResult<Session>;
 
-    async fn access(
+    async fn get_active_sessions_count(
+        &self,
+        user_id: &UserKey,
+        account_id: &AccountKey,
+    ) -> RepoResult<usize>;
+
+    async fn get_optional_valid_by_token(
+        &self,
+        token: &str,
+        unique_identifier: &str,
+    ) -> RepoResult<Option<Session>>;
+
+    async fn update(
         &self,
         id: &SessionKey,
         address: Option<String>,
         agent: &str,
+        validitiy: Duration,
     ) -> RepoResult<()>;
+
+    async fn create_for(
+        &self,
+        user_id: &UserKey,
+        account_id: &AccountKey,
+        insert: &InsertSession,
+    ) -> RepoResult<SessionKey>;
+
+    async fn remove(&self, id: &SessionKey) -> RepoResult<()>;
+}
+
+#[derive(Debug)]
+pub struct InsertSession {
+    pub device_identifier: String,
+    pub agent: String,
+    pub address: Option<String>,
+    pub valid_until: DateTime<Utc>,
+    pub refresh_token: String,
 }
