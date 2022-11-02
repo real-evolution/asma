0c7a2b28a446d590871bf29e276ba5c4525c16dc
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-02 20:23:53 +0300

    Add an experimental implementation for `AuthService`

--- crates/app/services/src/auth.rs
+++ crates/app/services/src/auth.rs
@@ -1,10 +1,14 @@
 use std::sync::Arc;
 
+use chrono::{Duration, Utc};
 use kernel_repositories::*;
 use kernel_services::auth::{models::DeviceInfo, AuthService};
-use kernel_services::error::AppResult;
-
+use kernel_services::crypto::hash::CryptoHashService;
+use kernel_services::error::{AppResult, AuthError};
 use shaku::Component;
+
+const MAX_SESSIONS_COUNT: usize = 10;
+const SESSION_VALIDITY_DAYS: i64 = 180;
 
 #[derive(Component)]
 #[shaku(interface = AuthService)]
@@ -16,10 +20,10 @@ pub struct AppAuthService {
     accounts: Arc<dyn AccountsRepo>,
 
     #[shaku(inject)]
-    roles: Arc<dyn AccountsRepo>,
+    sessions: Arc<dyn SessionsRepo>,
 
     #[shaku(inject)]
-    sessions: Arc<dyn AccountsRepo>,
+    hash_svc: Arc<dyn CryptoHashService>,
 }
 
 #[async_trait::async_trait]
@@ -27,11 +31,77 @@ impl AuthService for AppAuthService {
     async fn signin(
         &self,
         account_name: &str,
-        usrename: &str,
+        username: &str,
         password: &str,
         device_info: DeviceInfo,
     ) -> AppResult<()> {
-        todo!()
+        let user = self.users.get_by_username(username).await?;
+        let account = self
+            .accounts
+            .get_of_user_by_name(&user.id, account_name)
+            .await?;
+
+        if let Err(err) = self.hash_svc.verify(password, &account.password_hash)
+        {
+            warn!("could not verify password of `{account_name}@{username}`: {err}");
+            return Err(AuthError::InvalidCredentials.into());
+        }
+
+        if let Ok(session) = self
+            .sessions
+            .get_valid_for(
+                &user.id,
+                &account.id,
+                &device_info.device_identifier,
+            )
+            .await
+        {
+            self.sessions
+                .update(
+                    &session.id,
+                    device_info.last_address.or(session.last_address),
+                    &device_info.agent,
+                    Duration::days(SESSION_VALIDITY_DAYS),
+                )
+                .await?;
+
+            info!(
+                "`{}@{}` signed-in with a saved session `{:#?}`",
+                account_name, username, session.id
+            );
+
+            return Ok(());
+        }
+
+        if self
+            .sessions
+            .get_active_sessions_count(&user.id, &account.id)
+            .await?
+            >= MAX_SESSIONS_COUNT
+        {
+            warn!(
+                "`{}@{}` has reached maximum sessions acount of {}",
+                account_name, username, MAX_SESSIONS_COUNT
+            );
+
+            return Err(
+                AuthError::MaxSessionsCountReached(MAX_SESSIONS_COUNT).into()
+            );
+        }
+
+        let session = InsertSession {
+            device_identifier: device_info.device_identifier,
+            agent: device_info.agent,
+            address: device_info.last_address,
+            valid_until: Utc::now() + Duration::days(SESSION_VALIDITY_DAYS),
+            refresh_token: "test token".into(),
+        };
+
+        self.sessions
+            .create_for(&user.id, &account.id, &session)
+            .await?;
+
+        Ok(())
     }
 
     async fn refresh_session(
@@ -39,7 +109,25 @@ impl AuthService for AppAuthService {
         refresh_token: &str,
         device_info: DeviceInfo,
     ) -> AppResult<()> {
-        todo!()
+        let session = self
+            .sessions
+            .get_optional_valid_by_token(
+                refresh_token,
+                &device_info.device_identifier,
+            )
+            .await?
+            .ok_or(AuthError::NotAuthenticated)?;
+
+        self.sessions
+            .update(
+                &session.id,
+                device_info.last_address.or(session.last_address),
+                &device_info.agent,
+                Duration::days(SESSION_VALIDITY_DAYS),
+            )
+            .await?;
+
+        Ok(())
     }
 
     async fn invalidate_session(
@@ -47,6 +135,12 @@ impl AuthService for AppAuthService {
         refresh_token: &str,
         device_identifier: &str,
     ) -> AppResult<()> {
-        todo!()
+        let session = self
+            .sessions
+            .get_optional_valid_by_token(refresh_token, device_identifier)
+            .await?
+            .ok_or(AuthError::NotAuthenticated)?;
+
+        Ok(self.sessions.remove(&session.id).await?)
     }
 }
