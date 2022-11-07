f4210f19b47d793c02aebc921cae05a341582ad5
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-07 06:33:36 +0300

    Remove level

--- crates/kernel/entities/src/entities/user.rs
+++ crates/kernel/entities/src/entities/user.rs
@@ -10,19 +10,10 @@ pub enum UserState {
     Suspended = 2,
 }
 
-#[derive(sqlx::Type)]
-#[repr(i32)]
-pub enum UserLevel {
-    Root = 2,
-    Admin = 1,
-    Normal = 0,
-}
-
 #[entity]
 #[derive(sqlx::FromRow)]
 pub struct User {
     pub display_name: String,
     pub username: String,
-    pub level: UserLevel,
     pub state: UserState,
 }
