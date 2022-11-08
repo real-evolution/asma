27fdf51ccdc240267f096942f5a5430ba9fe11d7
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-09 02:26:40 +0300

    Add a method to get all user swith a certain type

--- crates/kernel/repositories/src/users.rs
+++ crates/kernel/repositories/src/users.rs
@@ -1,10 +1,11 @@
-use crate::error::RepoResult;
 use kernel_entities::entities::*;
-
 use shaku::Interface;
+
+use crate::error::RepoResult;
 
 #[async_trait::async_trait]
 pub trait UsersRepo: Interface {
     async fn get_by_id(&self, id: &UserKey) -> RepoResult<User>;
     async fn get_by_username(&self, username: &str) -> RepoResult<User>;
+    async fn get_all_by_level(&self, level: UserLevel) -> RepoResult<Vec<User>>;
 }
