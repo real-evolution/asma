3f17cad1ae7f908153505b093d3aed36db8dcd26
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-09 02:26:15 +0300

    Add a field to hold user type

--- crates/kernel/entities/src/entities/user.rs
+++ crates/kernel/entities/src/entities/user.rs
@@ -10,10 +10,19 @@ pub enum UserState {
     Suspended = 2,
 }
 
+#[derive(sqlx::Type)]
+#[repr(i32)]
+pub enum UserLevel {
+    Root = 0,
+    Admin = 1,
+    Regular = 2,
+}
+
 #[entity]
 #[derive(sqlx::FromRow)]
 pub struct User {
     pub display_name: String,
     pub username: String,
     pub state: UserState,
+    pub level: UserLevel,
 }
