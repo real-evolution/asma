3d33a936b0b743c4c901a7291bb10ca58c3bbea6
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-10 01:45:51 +0300

    Export `TrasactionsManager` service through DI

--- crates/kernel/repositories/src/di.rs
+++ crates/kernel/repositories/src/di.rs
@@ -1,9 +1,10 @@
-use crate::*;
-
 use shaku::HasComponent;
 
+use crate::*;
+
 pub trait ReposModule:
-    HasComponent<dyn UsersRepo>
+    HasComponent<dyn TransactionManager>
+    + HasComponent<dyn UsersRepo>
     + HasComponent<dyn AccountsRepo>
     + HasComponent<dyn RolesRepo>
     + HasComponent<dyn SessionsRepo>
