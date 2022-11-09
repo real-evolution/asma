8fac8fa48508158cfab3875cc86912de2077b586
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-09 04:26:51 +0300

    Merge related entities' modules

--- crates/kernel/entities/src/entities/mod.rs
+++ crates/kernel/entities/src/entities/mod.rs
@@ -1,13 +1,9 @@
 mod account;
-mod account_role;
-mod oauth2_login;
 mod role;
 mod session;
 mod user;
 
 pub use account::*;
-pub use account_role::*;
-pub use oauth2_login::*;
 pub use role::*;
 pub use user::*;
 pub use session::*;
--- crates/kernel/entities/src/entities/role.rs
+++ crates/kernel/entities/src/entities/role.rs
@@ -1,9 +1,19 @@
-use crate::traits::*;
 use kernel_proc_macros::*;
+
+use super::AccountKey;
+use crate::traits::*;
 
 #[entity]
 #[derive(Debug, Clone, sqlx::FromRow)]
 pub struct Role {
     pub code: String,
     pub friendly_name: Option<String>,
+}
+
+#[entity(entity_type = "immutable")]
+#[derive(Debug, Clone, sqlx::FromRow)]
+pub struct AccountRole {
+    pub account_id: AccountKey,
+    pub role_id: RoleKey,
+    pub enabled: bool,
 }
