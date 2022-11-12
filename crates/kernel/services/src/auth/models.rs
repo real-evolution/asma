b2598f6a74aef69446314ef84d0fc73589476d72
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-12 04:17:00 +0300

    Add a model to represent an access rule (role, permissions)

--- crates/kernel/services/src/auth/models.rs
+++ crates/kernel/services/src/auth/models.rs
@@ -1,3 +1,5 @@
+use derive_more::Constructor;
+use kernel_entities::entities::auth::{Resource, Actions};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -5,4 +7,10 @@ pub struct DeviceInfo {
     pub device_identifier: String,
     pub agent: String,
     pub last_address: String,
+}
+
+#[derive(Debug, Constructor)]
+pub struct AccessRule {
+    pub role_code: String,
+    pub permissions: Vec<(Resource, Actions)>
 }
