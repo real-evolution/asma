99541e8a35bb21f28642921f2d646b7097865f9b
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-19 04:09:47 +0300

    Add an extractor to get pagination parameters

---
+++ crates/driver/web_rest/src/api/dtos/pagination.rs
@@ -0,0 +1,25 @@
+use chrono::{DateTime, Utc};
+use common_macros::into_fn;
+use serde::Deserialize;
+use utoipa::IntoParams;
+use validator::Validate;
+
+into_fn!(default_before_timestamp: DateTime<Utc> =>  Utc::now());
+into_fn!(default_page_size: usize =>  32usize);
+
+#[derive(Deserialize, IntoParams, Validate)]
+#[serde(rename_all = "camelCase")]
+pub struct Pagination {
+    #[serde(default = "default_before_timestamp")]
+    pub before: DateTime<Utc>,
+
+    #[serde(default = "default_page_size")]
+    #[validate(range(min = 1, max = 128))]
+    pub page_size: usize,
+}
+
+impl Into<(DateTime<Utc>, usize)> for Pagination {
+    fn into(self) -> (DateTime<Utc>, usize) {
+        (self.before, self.page_size)
+    }
+}
