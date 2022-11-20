a026950bc16fb0a5582186f82dfbe20518df58b7
Ayman Al-Qadhi <alqd@protonmail.com>
2022-11-20 05:14:37 +0300

    Fix data type

--- crates/kernel/entities/src/entities/auth/session.rs
+++ crates/kernel/entities/src/entities/auth/session.rs
@@ -11,7 +11,7 @@ pub struct Session {
     pub device_identifier: String,
     pub agent: String,
     pub refresh_token: String,
-    pub last_address: Option<String>,
+    pub last_address: String,
     pub account_id: AccountKey,
     pub expires_at: Option<DateTime<Utc>>,
 }
