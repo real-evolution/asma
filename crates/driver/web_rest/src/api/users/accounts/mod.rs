9b9c3afd23505991a21f9aa2bf75de8b9b533bbf
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-04 16:55:00 +0300

    Add user account removal handler

--- crates/driver/web_rest/src/api/users/accounts/mod.rs
+++ crates/driver/web_rest/src/api/users/accounts/mod.rs
@@ -1,8 +1,10 @@
 pub mod view;
+pub mod remove;
 
 use axum::{routing::get, Router};
 
 pub fn routes() -> Router {
     Router::new()
         .route("/", get(view::get_all_of))
+        .route("/:account_id", get(view::get_of_by_id))
 }
---
+++ crates/driver/web_rest/src/api/users/accounts/remove.rs
@@ -0,0 +1,27 @@
+use axum::extract::Path;
+use kernel_entities::entities::auth::{Account, Action, KnownRoles, Resource};
+use kernel_entities::traits::Key;
+use kernel_repositories::auth::AccountsRepo;
+
+use crate::{error::ApiResult, extractors::di::Dep, util::claims::Claims};
+
+#[utoipa::path(
+    delete,
+    path = "/api/users/{user_id}/accounts/{account_id}",
+    responses((status = 200, description = "Account removed")),
+    responses((status = 404, description = "User or account not found")),
+    params(
+        ("user_id" = Key<Account>, Path, description = "Id of the user to remove the account of"),
+        ("account_id" = Key<Account>, Path, description = "Id of the account to remove"),
+    )
+)]
+pub async fn remove(
+    claims: Claims,
+    user_id: Path<Key<User>>,
+    account_id: Path<Key<Account>>,
+    accounts_repo: Dep<dyn AccountsRepo>,
+) -> ApiResult<()> {
+    claims.of_with(&user_id, &[(Resource::Accounts, Action::Remove)])?;
+
+    Ok(accounts_repo.remove_for(&account_id, &user_id).await?)
+}
