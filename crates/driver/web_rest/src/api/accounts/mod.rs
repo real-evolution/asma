372a7f479312a39b12e137dc89ff6ac5df90c7b4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-04 16:55:14 +0300

    Remove handler reigstration

--- crates/driver/web_rest/src/api/accounts/mod.rs
+++ crates/driver/web_rest/src/api/accounts/mod.rs
@@ -8,9 +8,6 @@ use axum::{routing::*, Router};
 pub fn routes() -> Router {
     Router::new()
         .route("/", get(view::get_all))
-        .route(
-            "/:account_id",
-            get(view::get_by_id).post(add::add).delete(remove::remove),
-        )
+        .route("/:account_id", get(view::get_by_id).post(add::add))
         .route("/password", put(update::update_password))
 }
