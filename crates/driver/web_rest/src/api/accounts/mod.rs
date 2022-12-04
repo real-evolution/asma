e5e8ede6e2c0c548c7cf86b255aa4e5db05d95a4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-04 16:57:26 +0300

    Remove unneeded handler

--- crates/driver/web_rest/src/api/accounts/mod.rs
+++ crates/driver/web_rest/src/api/accounts/mod.rs
@@ -1,13 +1,11 @@
 pub mod add;
 pub mod dtos;
 pub mod update;
-pub mod view;
 
 use axum::{routing::*, Router};
 
 pub fn routes() -> Router {
     Router::new()
-        .route("/", get(view::get_all))
         .route("/:account_id", get(view::get_by_id).post(add::add))
         .route("/password", put(update::update_password))
 }
