91a428a8ffe7831025e3912ca296e7e3dcb68543
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-04 16:38:29 +0300

    Add a trait to represent a child repository

--- crates/kernel/repositories/src/traits/repo.rs
+++ crates/kernel/repositories/src/traits/repo.rs
@@ -19,3 +19,22 @@ pub trait Repo<E: Entity> {
 pub trait InsertRepo<E: Entity, I> {
     async fn create(&self, model: I) -> RepoResult<E>;
 }
+
+#[async_trait::async_trait]
+pub trait ChildRepo<E: Entity, P: Entity> {
+    async fn get_paginated_for(
+        &self,
+        parent_key: &Key<P>,
+        before: &DateTime<Utc>,
+        limit: usize,
+    ) -> RepoResult<Vec<E>>;
+
+    async fn get_for(&self, key: &Key<E>, parent_key: &Key<P>)
+        -> RepoResult<E>;
+
+    async fn remove_for(
+        &self,
+        key: &Key<E>,
+        parent_key: &Key<P>,
+    ) -> RepoResult<()>;
+}
