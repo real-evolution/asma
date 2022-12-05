90475f6947b1735217b41b469626cb12681b33e4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-05 20:08:14 +0300

    Write an implementation for `LinkDataStore` trait

--- crates/adapter/repositories_sqlx/src/link/mod.rs
+++ crates/adapter/repositories_sqlx/src/link/mod.rs
@@ -1,3 +1,23 @@
 mod channels;
 
-pub use channels::*;
+use kernel_repositories::link::*;
+
+use crate::database::SqlxPool;
+
+pub(crate) struct SqlxLinkDataStore {
+    channels: channels::SqlxChannelsRepo,
+}
+
+impl SqlxLinkDataStore {
+    pub(crate) fn new(pool: SqlxPool) -> Self {
+        Self {
+            channels: channels::SqlxChannelsRepo(pool.clone()),
+        }
+    }
+}
+
+impl LinkDataStore for SqlxLinkDataStore {
+    fn channels(&self) -> &dyn ChannelsRepo {
+        &self.channels
+    }
+}
