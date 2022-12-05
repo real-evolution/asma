afe94df521748db95011032efa5738d4319081b5
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-05 20:09:02 +0300

    Write an implementation for `DataStore` trait

--- crates/adapter/repositories_sqlx/src/lib.rs
+++ crates/adapter/repositories_sqlx/src/lib.rs
@@ -1,6 +1,35 @@
+use std::sync::Arc;
+
+use auth::SqlxAuthDataStore;
+use database::SqlxPool;
+use kernel_repositories::{auth::AuthDataStore, link::LinkDataStore, *};
+use link::SqlxLinkDataStore;
+
+mod auth;
+mod config;
+mod database;
+mod link;
 mod util;
 
-pub mod auth;
-pub mod config;
-pub mod database;
-pub mod link;
+pub use config::*;
+
+struct SqlxDataStore {
+    pool: SqlxPool,
+    auth: SqlxAuthDataStore,
+    link: SqlxLinkDataStore,
+}
+
+impl DataStore for SqlxDataStore {
+    fn tx(&self) -> &dyn TransactionManager {
+        &self.pool
+    }
+
+    fn auth(&self) -> &dyn AuthDataStore {
+        &self.auth
+    }
+
+    fn link(&self) -> &dyn LinkDataStore {
+        &self.link
+    }
+}
+
