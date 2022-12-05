e2f0fa07c164b86220d6a7705200a81fc2ff2b39
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-05 20:01:11 +0300

    Add a trait to aggregate auth repositories

--- crates/kernel/repositories/src/auth/mod.rs
+++ crates/kernel/repositories/src/auth/mod.rs
@@ -7,3 +7,10 @@ pub use accounts::*;
 pub use roles::*;
 pub use sessions::*;
 pub use users::*;
+
+pub trait AuthDataStore: Send + Sync {
+    fn users(&self) -> &dyn UsersRepo;
+    fn roles(&self) -> &dyn RolesRepo;
+    fn accounts(&self) -> &dyn AccountsRepo;
+    fn sessions(&self) -> &dyn SessionsRepo;
+}
