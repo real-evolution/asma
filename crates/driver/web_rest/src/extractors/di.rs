a9512874abdf4a57509caac7ec834b69a7e59b05
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-05 20:18:49 +0300

    Remove `Dep` extractor

--- crates/driver/web_rest/src/extractors/di.rs
+++
@@ -1,4 +0,0 @@
-use driver_web_common::di::DI;
-use shaku_axum::Inject;
-
-pub(crate) type Dep<T> = Inject<dyn DI, T>;
--- crates/driver/web_rest/src/extractors/mod.rs
+++ crates/driver/web_rest/src/extractors/mod.rs
@@ -1,5 +1,4 @@
 pub(crate) mod claims;
-pub(crate) mod di;
 pub(crate) mod validated_json;
 pub(crate) mod validated_query;
 pub(crate) mod config;
