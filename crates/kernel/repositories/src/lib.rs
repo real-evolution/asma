192e114133dc1b6b261a5f17a743725cb147ea04
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-05 20:01:37 +0300

    Add a trait to aggregate data access objects

--- crates/kernel/repositories/src/lib.rs
+++ crates/kernel/repositories/src/lib.rs
@@ -3,8 +3,13 @@ pub mod traits;
 
 pub use database_connection::*;
 
+pub mod auth;
 pub mod di;
 pub mod error;
-
-pub mod auth;
 pub mod link;
+
+pub trait DataStore: Send + Sync {
+    fn tx(&self) -> &dyn TransactionManager;
+    fn auth(&self) -> &dyn auth::AuthDataStore;
+    fn link(&self) -> &dyn link::LinkDataStore;
+}
