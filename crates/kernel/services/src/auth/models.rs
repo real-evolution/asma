d812b4d16cc928a061ec00d6c8bb3eafe3801656
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-08 17:38:02 +0300

    Remove `AccessRole` model

--- crates/kernel/services/src/auth/models.rs
+++ crates/kernel/services/src/auth/models.rs
@@ -1,16 +1,8 @@
-use derive_more::Constructor;
-use kernel_entities::entities::auth::*;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct DeviceInfo {
     pub device_identifier: String,
     pub agent: String,
     pub last_address: String,
-}
-
-#[derive(Clone, Constructor, Debug, Deserialize, Serialize)]
-pub struct AccessRule {
-    pub role_code: String,
-    pub permissions: Vec<(Resource, Actions)>,
 }
