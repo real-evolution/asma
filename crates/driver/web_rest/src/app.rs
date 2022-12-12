635c017881c7786f9da64f52a70aed0bd637fa97
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-12 17:23:31 +0300

    Add redoc support

--- crates/driver/web_rest/src/app.rs
+++ crates/driver/web_rest/src/app.rs
@@ -1,6 +1,7 @@
 use aide::{
     axum::{routing::get, ApiRouter, IntoApiResponse},
     openapi::{Info, OpenApi},
+    redoc::Redoc,
 };
 use axum::{Extension, Router};
 use cached::proc_macro::once;
@@ -8,6 +9,8 @@ use driver_web_common::state::AppState;
 use kernel_services::error::AppResult;
 
 use crate::api;
+
+const OPENAPI_PATH: &str = "/openapi.json";
 
 #[once]
 fn serialize_api(api: &OpenApi) -> String {
@@ -21,15 +24,23 @@ async fn serve_api(Extension(api): Extension<OpenApi>) -> impl IntoApiResponse {
 pub fn make_app() -> AppResult<Router<AppState>> {
     let mut api = OpenApi {
         info: Info {
-            description: Some("an example API".to_string()),
-            ..Info::default()
+            title: "asma".into(),
+            description: Some("Advanced Social Media Aggregator".into()),
+            version: "0.0.1-dev".into(),
+            ..Default::default()
         },
-        ..OpenApi::default()
+        ..Default::default()
     };
 
     Ok(ApiRouter::new()
-        .route("/api.json", get(serve_api))
+        .route(
+            "/redoc",
+            Redoc::new(OPENAPI_PATH)
+                .axum_route()
+                .with_state::<AppState>(()),
+        )
         .nest("/api", api::api_routes()?)
+        .route(OPENAPI_PATH, get(serve_api))
         .finish_api(&mut api)
         .layer(Extension(api)))
 }
