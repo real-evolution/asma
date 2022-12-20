181277a133a31d9fd44a59e2bf7e277fb083a29c
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-20 21:20:52 +0300

    Rewrite state building code to use the new `Service` trait

--- crates/driver/web_common/src/state.rs
+++ crates/driver/web_common/src/state.rs
@@ -1,21 +1,27 @@
 use std::sync::Arc;
 
 use adapter_repositories_postgres::*;
-use adapter_services::config::TomlConfigService;
-use adapter_services::crypto::hash::Argon2CryptoHashService;
-use adapter_services::entropy::SecureEntropyService;
-use app_services::auth::config::{AuthConfig, AUTH_CONFIG_SECTION};
-use app_services::auth::AppAuthService;
-use app_services::link::channels::AppChannelsService;
-use app_services::setup::AppSetupService;
+use adapter_services::{
+    config::TomlConfigService,
+    crypto::hash::Argon2CryptoHashService,
+    entropy::SecureEntropyService,
+};
+use app_services::{
+    auth::AppAuthService,
+    link::channels::AppChannelsService,
+    setup::AppSetupService,
+};
 use kernel_repositories::DataStore;
-use kernel_services::auth::AuthService;
-use kernel_services::config::ConfigService;
-use kernel_services::crypto::hash::CryptoHashService;
-use kernel_services::entropy::EntropyService;
-use kernel_services::get_config;
-use kernel_services::link::channels::ChannelsService;
-use kernel_services::setup::SetupService;
+use kernel_services::{
+    auth::AuthService,
+    config::ConfigService,
+    crypto::hash::CryptoHashService,
+    entropy::EntropyService,
+    get_config,
+    link::channels::ChannelsService,
+    setup::SetupService,
+    Service,
+};
 
 pub type AppState = Arc<AppStateImpl>;
 
@@ -34,35 +40,25 @@ pub struct AppStateImpl {
 }
 
 pub async fn create_state() -> anyhow::Result<AppState> {
-    debug!(
-        "loading config from toml files: {:?}",
-        TomlConfigService::get_config_files()?
-    );
-
-    let config = TomlConfigService::load()?;
+    let config = init(TomlConfigService::default()).await?;
 
     debug!("creating datastore");
     let conf = get_config!(config, DATA_CONFIG_SECTION => DataConfig)?;
     let data = create_datastore(conf).await?;
 
     debug!("creating base services");
-    let config = Arc::new(config);
-    let entropy = Arc::new(SecureEntropyService::default());
-    let hash = Arc::new(Argon2CryptoHashService::new());
+    let entropy = init(SecureEntropyService::default()).await?;
+    let hash = init(Argon2CryptoHashService::new()).await?;
 
     debug!("creating app services");
-    // auth
-    let conf = get_config!(config, AUTH_CONFIG_SECTION => AuthConfig)?;
     let auth = Arc::new(AppAuthService::new(
-        conf,
         data.clone(),
+        config.clone(),
         hash.clone(),
         entropy.clone(),
     ));
-    // setup
-    let setup = Arc::new(AppSetupService::new(data.clone(), auth.clone()));
-    // channels
-    let channels = Arc::new(AppChannelsService::new(data.clone()));
+    let setup = init(AppSetupService::new(data.clone(), auth.clone())).await?;
+    let channels = init(AppChannelsService::new(data.clone())).await?;
 
     debug!("building application state");
     Ok(Arc::new(AppStateImpl {
@@ -74,4 +70,10 @@ pub async fn create_state() -> anyhow::Result<AppState> {
         setup,
         channels,
     }))
+}
+
+async fn init<S: Service + Send + Sync>(svc: S) -> anyhow::Result<Arc<S>> {
+    svc.initialize().await?;
+
+    Ok(Arc::new(svc))
 }
