bf6b2e24fe31bb11fccbe9112226460aec5e860f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-27 23:48:17 +0300

    Add a validator to validate supported message queue types

--- crates/common/validation/src/constants.rs
+++ crates/common/validation/src/constants.rs
@@ -1,1 +1,2 @@
 pub(crate) const SUPPORTED_DATA_DRIVERS: [&str; 1] = ["postgres"];
+pub(crate) const SUPPORTED_MESSAGE_QUEUE_PROTOCOLS: [&str; 1] = ["amqp"];
--- crates/common/validation/src/custom.rs
+++ crates/common/validation/src/custom.rs
@@ -2,6 +2,7 @@ use chrono::{DateTime, Utc};
 use validator::ValidationError;
 
 use crate::{
+    constants::{SUPPORTED_DATA_DRIVERS, SUPPORTED_MESSAGE_QUEUE_PROTOCOLS},
     helpers::{validate, validate_with},
     parse::*,
 };
@@ -31,6 +32,13 @@ pub fn supported_data_driver(value: &str) -> Result<(), ValidationError> {
     })
 }
 
+pub fn supported_message_queue_protocol(
+    value: &str,
+) -> Result<(), ValidationError> {
+    validate_with("supported_message_queue_protocol", value, |v| {
+        SUPPORTED_MESSAGE_QUEUE_PROTOCOLS.contains(&v)
+    })
+}
 
 pub fn in_future(value: &DateTime<Utc>) -> Result<(), ValidationError> {
     validate_with("in_future", value, |ts| ts < &Utc::now())
