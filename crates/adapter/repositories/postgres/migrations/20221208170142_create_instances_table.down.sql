4ceca0289ec0fd69a9d2313887c7fc24905ca4de
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-02 16:57:00 +0300

    Add indexes to instances table

--- crates/adapter/repositories/postgres/migrations/20221208170142_create_instances_table.down.sql
+++ crates/adapter/repositories/postgres/migrations/20221208170142_create_instances_table.down.sql
@@ -1,2 +1,4 @@
 DROP INDEX instances_created_at_idx;
+DROP INDEX instances_platform_identifier_idx;
+DROP INDEX instances_platform_username_idx;
 DROP TABLE instances;
--- crates/adapter/repositories/postgres/migrations/20221208170142_create_instances_table.up.sql
+++ crates/adapter/repositories/postgres/migrations/20221208170142_create_instances_table.up.sql
@@ -2,7 +2,8 @@ CREATE TABLE instances
 (
     id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
 
-    platform_identifier VARCHAR NOT NULL,
+    platform_identifier BIGINT NOT NULL,
+    platform_username VARCHAR NOT NULL,
 
     display_name VARCHAR NULL,
     phone_number VARCHAR NULL,
@@ -11,8 +12,11 @@ CREATE TABLE instances
     channel_id UUID NOT NULL,
 
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
 
     CONSTRAINT channel_fk FOREIGN KEY (channel_id) REFERENCES channels(id)
 );
 
 CREATE INDEX instances_created_at_idx ON channels USING btree (created_at);
+CREATE INDEX instances_platform_identifier_idx ON channels USING btree (created_at);
+CREATE INDEX instances_platform_username_idx ON channels USING btree (created_at);
