975c9a907e12b2543037323358b7186c10a87c50
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-02 22:36:57 +0300

    Rewrite `MessagePassingService` trait to be topic-first

--- crates/kernel/services/src/link/message_passing.rs
+++ crates/kernel/services/src/link/message_passing.rs
@@ -7,36 +7,42 @@ use serde::{de::DeserializeOwned, Serialize};
 use crate::error::AppResult;
 
 #[async_trait]
-pub trait MessagePassingService: Send + Sync {
-    async fn publish_raw(
-        &self,
-        topic: &str,
-        key: Option<&str>,
-        body: &[u8],
-    ) -> AppResult<()>;
-
-    async fn publish<T: Serialize + Send + Sync>(
-        &self,
-        topic: &str,
-        key: Option<&str>,
-        body: &T,
-    ) -> AppResult<()>;
-
-    fn subscribe<'a, T: DeserializeOwned + Send + 'a>(
-        &'a self,
-        topic: &'a str,
-        key: Option<&'a str>,
-    ) -> BoxStream<'a, AppResult<T>>;
-
-    fn subscribe_manual<'a, T: DeserializeOwned + Send + 'a>(
-        &'a self,
-        topic: &'a str,
-        key: Option<&'a str>,
-    ) -> BoxStream<'a, AppResult<(T, Arc<dyn MessageConfirmation>)>>;
+pub trait MessagePassingService<T>: Send + Sync {
+    async fn get_topic(&self, name: &str) -> AppResult<Arc<T>>;
 }
 
 #[async_trait]
 pub trait MessageConfirmation: Send + Sync + 'static {
     async fn ack(self) -> AppResult<()>;
     async fn nack(self, requeue: bool) -> AppResult<()>;
+}
+
+#[async_trait]
+pub trait Topic {
+    async fn publish<T: Serialize + Send + Sync>(
+        &self,
+        key: Option<&str>,
+        body: &T,
+    ) -> AppResult<()>;
+
+    async fn publish_confirmed<T: Serialize + Send + Sync>(
+        &self,
+        key: Option<&str>,
+        body: &T,
+    ) -> AppResult<()>;
+
+    async fn subscribe<'a, T: DeserializeOwned + Send + 'a>(
+        &'a self,
+        key: Option<&'a str>,
+    ) -> AppResult<BoxStream<'a, AppResult<T>>>;
+
+    async fn subscribe_manual<'a, T: DeserializeOwned + Send + 'a>(
+        &'a self,
+        key: Option<&'a str>,
+    ) -> AppResult<BoxStream<'a, AppResult<(T, Arc<dyn MessageConfirmation>)>>>;
+
+    async fn mirror<'a, T: DeserializeOwned + Send + 'a>(
+        &'a self,
+        key: Option<&'a str>,
+    ) -> AppResult<BoxStream<'a, AppResult<T>>>;
 }
