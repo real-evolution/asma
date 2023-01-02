f53a746e6f5060506f10e2f21729aef61e2e8d69
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-02 17:15:00 +0300

    Add serde derive macros

--- crates/kernel/entities/src/entities/comm/attachment.rs
+++ crates/kernel/entities/src/entities/comm/attachment.rs
@@ -1,13 +1,14 @@
 use schemars::JsonSchema;
+use serde::{Serialize, Deserialize};
 
-#[derive(Clone, Debug, JsonSchema)]
+#[derive(Clone, Debug, JsonSchema, Serialize, Deserialize)]
 pub enum AttachmentKind {
     Document,
     Audio,
     Video,
 }
 
-#[derive(Clone, Debug, JsonSchema)]
+#[derive(Clone, Debug, JsonSchema, Serialize, Deserialize)]
 pub struct Attachment {
     pub kind: AttachmentKind,
     pub label: Option<String>,
--- crates/kernel/entities/src/entities/comm/message.rs
+++ crates/kernel/entities/src/entities/comm/message.rs
@@ -1,50 +1,40 @@
 use chrono::{DateTime, Utc};
+use kernel_proc_macros::entity;
 use schemars::JsonSchema;
+use serde::{Serialize, Deserialize};
 
 use crate::traits::{Entity, Key};
 
 use super::{attachment::Attachment, chat::Chat};
 
-#[derive(Clone, Debug, JsonSchema)]
+#[derive(Clone, Debug, JsonSchema, Serialize, Deserialize)]
 pub enum MessageModificationKind {
     Edit(String),
     Delete,
 }
 
-#[derive(Clone, Debug, JsonSchema)]
+#[derive(Clone, Debug, JsonSchema, Serialize, Deserialize)]
 pub struct MessageModification {
     pub kind: MessageModificationKind,
     pub made_at: DateTime<Utc>,
 }
 
-#[derive(Clone, Debug, JsonSchema)]
+#[derive(Clone, Debug, JsonSchema, Serialize, Deserialize)]
 pub enum MessageDirection {
     Incoming,
     Outgoing,
 }
 
+#[entity(entity_type = "immutable")]
 #[derive(Clone, Debug, JsonSchema)]
 pub struct Message {
-    pub id: Key<Message>,
-
     pub text: Option<String>,
     pub changes: Option<Vec<MessageModification>>,
     pub attachments: Option<Vec<Attachment>>,
     pub direction: MessageDirection,
 
-    pub sent_at: DateTime<Utc>,
     pub delivered_at: Option<DateTime<Utc>>,
     pub seen_at: Option<DateTime<Utc>>,
 
     pub chat_id: Key<Chat>,
-}
-
-impl Entity for Message {
-    fn id(&self) -> &Key<Self> {
-        &self.id
-    }
-
-    fn created_at(&self) -> DateTime<Utc> {
-        self.sent_at
-    }
 }
