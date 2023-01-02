ab3dd04d035b36b2af7222f5f4d21af52008822b
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-02 17:03:29 +0300

    Add instance querying methods

--- crates/kernel/repositories/src/link/instances.rs
+++ crates/kernel/repositories/src/link/instances.rs
@@ -1,6 +1,35 @@
-use kernel_entities::entities::link::*;
+use derive_more::Constructor;
+use kernel_entities::{entities::link::*, traits::Key};
 
-use crate::traits::*;
+use crate::{error::RepoResult, traits::*};
 
 #[async_trait::async_trait]
-pub trait InstancesRepo: Repo<Entity = Instance> + Send + Sync {}
+pub trait InstancesRepo:
+    Repo<Entity = Instance> + InsertRepo<InsertInstance> + Send + Sync
+{
+    async fn get_by_platform_identifier(
+        &self,
+        channel_id: &Key<Channel>,
+        identifier: i64,
+    ) -> RepoResult<Instance>;
+
+    async fn get_by_platform_username(
+        &self,
+        channel_id: &Key<Channel>,
+        username: &str,
+    ) -> RepoResult<Instance>;
+
+    async fn get_all(
+        &self,
+        channel_id: &Key<Channel>,
+    ) -> RepoResult<Vec<Instance>>;
+}
+
+#[derive(Constructor)]
+pub struct InsertInstance {
+    pub channel_id: Key<Channel>,
+    pub platform_identifier: i64,
+    pub platform_username: String,
+    pub display_name: Option<String>,
+    pub phone_number: Option<String>,
+}
