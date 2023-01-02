e0995d9486de4ad6fde4d514fb4ab717302b3fd2
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-02 22:36:29 +0300

    Add channel update types

--- crates/kernel/services/src/link/channels.rs
+++ crates/kernel/services/src/link/channels.rs
@@ -39,3 +39,39 @@ pub trait ChannelsService: Send + Sync {
 pub struct ChannelStatus {
     pub started_at: DateTime<Utc>,
 }
+
+#[derive(Debug)]
+pub enum MessageUpdateKind {
+    New {
+        platform_message_id: Option<String>,
+        content: Option<String>,
+    },
+
+    Edit {
+        platform_message_id: String,
+        content: Option<String>,
+    },
+
+    Delete {
+        platform_message_id: String,
+    },
+}
+
+#[derive(Debug)]
+pub struct MessageUpdate {
+    pub chat_id: String,
+    pub by_id: String,
+    pub kind: MessageUpdateKind,
+    pub sent_at: DateTime<Utc>,
+}
+
+#[derive(Debug)]
+pub enum ChannelUpdateKind {
+    Message(MessageUpdate),
+}
+
+pub struct ChannelUpdate {
+    pub user_id: Key<User>,
+    pub channel_id: Key<Channel>,
+    pub kind: ChannelUpdateKind,
+}
