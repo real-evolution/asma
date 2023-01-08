9485b0884ee32f7e5fe70951ae098d2026d2bda8
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-08 21:43:07 +0300

    Add supported document stores drivers

--- crates/common/validation/src/constants.rs
+++ crates/common/validation/src/constants.rs
@@ -1,2 +1,3 @@
 pub(crate) const SUPPORTED_DATA_DRIVERS: [&str; 1] = ["postgres"];
+pub(crate) const SUPPORTED_DOC_STORE_DRIVERS: [&str; 1] = ["mongodb"];
 pub(crate) const SUPPORTED_MESSAGE_QUEUE_PROTOCOLS: [&str; 1] = ["amqp"];
