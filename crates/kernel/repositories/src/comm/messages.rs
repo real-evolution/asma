af6e2560431c1e27b0e8d91ed531b2fbf8692e2a
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-08 23:30:50 +0300

    Make `MessagesRepo` require `InsertRepo`

--- crates/kernel/repositories/src/comm/messages.rs
+++ crates/kernel/repositories/src/comm/messages.rs
@@ -1,6 +1,20 @@
-use kernel_entities::entities::comm::Message;
+use derive_more::Constructor;
+use kernel_entities::{
+    entities::comm::{Chat, Message, MessageDirection},
+    traits::Key,
+};
 
 use crate::traits::*;
 
 #[async_trait::async_trait]
-pub trait MessagesRepo: Repo<Entity = Message> + Send + Sync {}
+pub trait MessagesRepo:
+    Repo<Entity = Message> + InsertRepo<InsertMessage> + Send + Sync
+{
+}
+
+#[derive(Clone, Debug, Constructor)]
+pub struct InsertMessage {
+    pub chat_id: Key<Chat>,
+    pub text: Option<String>,
+    pub direction: MessageDirection,
+}
