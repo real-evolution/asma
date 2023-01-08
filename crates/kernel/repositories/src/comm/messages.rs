6c3d332ed8cb05fc2d71d4b58dd1163ce20a74f4
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-08 21:34:38 +0300

    Fix `MessagesRepo` trait specification

--- crates/kernel/repositories/src/comm/messages.rs
+++ crates/kernel/repositories/src/comm/messages.rs
@@ -1,6 +1,6 @@
-use kernel_entities::entities::comm::Chat;
+use kernel_entities::entities::comm::Message;
 
 use crate::traits::*;
 
 #[async_trait::async_trait]
-pub trait ChannelsRepo: Repo<Entity = Chat> + Send + Sync {}
+pub trait MessagesRepo: Repo<Entity = Message> + Send + Sync {}
