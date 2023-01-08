74711b5c27e210f847b1090c267ba45bd498dca1
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-08 23:30:25 +0300

    Rewrite `ChatsRepo` trait

--- crates/kernel/repositories/src/comm/chats.rs
+++ crates/kernel/repositories/src/comm/chats.rs
@@ -1,6 +1,23 @@
-use kernel_entities::entities::comm::Chat;
+use derive_more::Constructor;
+use kernel_entities::{
+    entities::{
+        auth::User,
+        comm::{Chat, ChatState},
+        link::{Channel, Instance},
+    },
+    traits::Key,
+};
 
 use crate::traits::*;
 
 #[async_trait::async_trait]
-pub trait ChannelsRepo: Repo<Entity = Chat> + Send + Sync {}
+pub trait ChatsRepo: Repo<Entity = Chat> + Send + Sync {}
+
+#[derive(Constructor)]
+pub struct InsertChat {
+    pub user_id: Key<User>,
+    pub label: Option<String>,
+    pub state: ChatState,
+    pub channel_id: Key<Channel>,
+    pub instance_id: Key<Instance>,
+}
