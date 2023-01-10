9424d3566edf16935f36ad90774c071d4e168468
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-10 22:38:08 +0300

    Use arrays instead of 'Option`

--- crates/kernel/entities/src/entities/comm/message.rs
+++ crates/kernel/entities/src/entities/comm/message.rs
@@ -17,11 +17,11 @@ pub enum MessageDirection {
 #[derive(Clone, Debug, JsonSchema)]
 pub struct Message {
     pub text: Option<String>,
-    pub attachments: Option<Vec<Attachment>>,
     pub changes: Vec<String>,
+    pub attachments: Vec<Attachment>,
     pub direction: MessageDirection,
 
-    pub delivered_at: Option<DateTime<Utc>>,
+    pub delivered_at: DateTime<Utc>,
     pub seen_at: Option<DateTime<Utc>>,
 
     pub chat_id: Key<Chat>,
