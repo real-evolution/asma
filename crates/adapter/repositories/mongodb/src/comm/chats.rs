140e5b13bad149f905ba27883a90759ddde7fc40
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-10 22:41:54 +0300

    Update repos implementation

--- crates/adapter/repositories/mongodb/src/comm/chats.rs
+++ crates/adapter/repositories/mongodb/src/comm/chats.rs
@@ -1,9 +1,39 @@
+use chrono::Utc;
 use kernel_entities::entities::comm::Chat;
-use kernel_repositories::comm::ChatsRepo;
+use kernel_repositories::{
+    comm::{ChatsRepo, InsertChat},
+    error::RepoResult,
+    traits::InsertRepo,
+};
 
-use crate::{repo::MongoDbRepo, traits::collection_entity::CollectionEntity};
+use crate::{
+    repo::MongoDbRepo, traits::collection_entity::CollectionEntity,
+    util::error::map_mongo_error,
+};
 
+#[async_trait::async_trait]
 impl ChatsRepo for MongoDbRepo<Chat> {}
+
+#[async_trait::async_trait]
+impl InsertRepo<InsertChat> for MongoDbRepo<Chat> {
+    async fn create(&self, model: InsertChat) -> RepoResult<Self::Entity> {
+        let chat = Chat {
+            id: uuid::Uuid::new_v4().into(),
+            label: model.label,
+            state: model.state,
+            user_id: model.user_id,
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+        };
+
+        self.collection()
+            .insert_one(&chat, None)
+            .await
+            .map_err(map_mongo_error)?;
+
+        Ok(chat)
+    }
+}
 
 impl CollectionEntity for Chat {
     fn name() -> &'static str {
--- crates/adapter/repositories/mongodb/src/comm/messages.rs
+++ crates/adapter/repositories/mongodb/src/comm/messages.rs
@@ -1,17 +1,39 @@
 use chrono::Utc;
-use kernel_entities::entities::comm::Message;
+use kernel_entities::{entities::comm::Message, traits::Key};
 use kernel_repositories::{
     comm::{InsertMessage, MessagesRepo},
     error::RepoResult,
-    traits::InsertRepo,
+    traits::{InsertRepo, Repo},
 };
+use mongodb::bson::doc;
 
 use crate::{
     repo::MongoDbRepo, traits::collection_entity::CollectionEntity,
     util::error::map_mongo_error,
 };
 
-impl MessagesRepo for MongoDbRepo<Message> {}
+#[async_trait::async_trait]
+impl MessagesRepo for MongoDbRepo<Message> {
+    async fn update_text(
+        &self,
+        id: &Key<Message>,
+        new_text: Option<String>,
+    ) -> RepoResult<Message> {
+        let message = self.get(id).await?;
+
+        self.collection().update_one(
+            doc! {"id": message.id.to_string()},
+            doc! {"$set": {
+                    "updated_at": Utc::now(),
+                    format!("changes.{}.content", message.changes.len()): new_text,
+                }
+            },
+            None,
+        ).await.map_err(map_mongo_error)?;
+
+        Ok(message)
+    }
+}
 
 #[async_trait::async_trait]
 impl InsertRepo<InsertMessage> for MongoDbRepo<Message> {
@@ -19,13 +41,16 @@ impl InsertRepo<InsertMessage> for MongoDbRepo<Message> {
         let message = Message {
             id: uuid::Uuid::new_v4().into(),
             text: model.text,
-            changes: None,
-            attachments: None,
+            changes: Vec::new(),
+            attachments: Vec::new(),
             direction: model.direction,
-            delivered_at: None,
+            delivered_at: model.delivered_at,
             seen_at: None,
             chat_id: model.chat_id,
+            instance_id: model.instance_id,
+            deleted_at: None,
             created_at: Utc::now(),
+            updated_at: Utc::now(),
         };
 
         self.collection()
