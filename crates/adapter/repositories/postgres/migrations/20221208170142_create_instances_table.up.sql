fa834fdf31f926574c234f0471d4cd7fbe7752ad
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-10 22:41:24 +0300

    Update migration

--- crates/adapter/repositories/postgres/migrations/20221208170142_create_instances_table.up.sql
+++ crates/adapter/repositories/postgres/migrations/20221208170142_create_instances_table.up.sql
@@ -3,17 +3,20 @@ CREATE TABLE instances
     id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
 
     platform_identifier BIGINT NOT NULL,
-    platform_username VARCHAR NOT NULL,
 
+    username VARCHAR NULL,
     display_name VARCHAR NULL,
     phone_number VARCHAR NULL,
 
     last_active TIMESTAMPTZ NULL,
+
+    chat_id UUID NOT NULL,
     channel_id UUID NOT NULL,
 
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
 
+    CONSTRAINT chat_fk UNIQUE (chat_id),
     CONSTRAINT channel_fk FOREIGN KEY (channel_id) REFERENCES channels(id)
 );
 
