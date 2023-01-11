a126617fac8813fff6667e4d8b647f3075998992
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-11 22:13:27 +0300

    Set `tonic` code generation options

--- crates/driver/web_grpc/build.rs
+++ crates/driver/web_grpc/build.rs
@@ -1,16 +1,20 @@
 fn main() -> anyhow::Result<()> {
-    tonic_build::configure().build_transport(true).compile(
-        &[
-            // models
-            "proto/models/user.proto",
-            "proto/models/chat.proto",
-            "proto/models/instance.proto",
-            "proto/models/message.proto",
-            // services
-            "proto/services/chats.proto",
-        ],
-        &["proto/"],
-    )?;
+    tonic_build::configure()
+        .build_transport(true)
+        .build_server(true)
+        .build_client(false)
+        .compile(
+            &[
+                // models
+                "proto/models/user.proto",
+                "proto/models/chat.proto",
+                "proto/models/instance.proto",
+                "proto/models/message.proto",
+                // services
+                "proto/services/chats.proto",
+            ],
+            &["proto/"],
+        )?;
 
     Ok(())
 }
