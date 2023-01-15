6b7390f51ef636746eb18c8bd985cb181a2f9cbf
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-15 05:16:49 +0300

    Split `listen_port` into two fields for REST and gRPC servers

--- crates/driver/web_runner/src/config/web.rs
+++ crates/driver/web_runner/src/config/web.rs
@@ -9,7 +9,8 @@ use validator::Validate;
 const CONFIG_SECTION: &str = "web";
 
 into_fn!(default_ip: String  => "127.0.0.1");
-into_fn!(default_port: const u16 => 3434u16);
+into_fn!(default_rest_port: const u16 => 3434u16);
+into_fn!(default_grpc_port: const u16 => 3435u16);
 
 #[derive(Debug, Deserialize, Validate)]
 struct LanuchConfig {
@@ -18,17 +19,27 @@ struct LanuchConfig {
     listen_address: String,
 
     #[validate(range(min = 0, max = 0xFFFF))]
-    #[serde(default = "default_port")]
-    listen_port: u16,
+    #[serde(default = "default_rest_port")]
+    rest_listen_port: u16,
+
+    #[validate(range(min = 0, max = 0xFFFF))]
+    #[serde(default = "default_grpc_port")]
+    grpc_listen_port: u16,
 }
 
-pub fn get_bind_address<C: ConfigService + ?Sized>(
-    svc: &C,
-) -> Result<SocketAddr> {
-    let conf: LanuchConfig = svc.get_section(CONFIG_SECTION)?;
+pub struct ListenAddressPair {
+    pub rest: SocketAddr,
+    pub grpc: SocketAddr,
+}
 
-    Ok(SocketAddr::new(
-        IpAddress::parse_str(conf.listen_address)?.0,
-        conf.listen_port,
-    ))
+impl ListenAddressPair {
+    pub fn load<C: ConfigService>(svc: &C) -> Result<Self> {
+        let conf: LanuchConfig = svc.get_section(CONFIG_SECTION)?;
+        let addr = IpAddress::parse_str(conf.listen_address)?.0;
+
+        Ok(Self {
+            rest: SocketAddr::new(addr, conf.rest_listen_port),
+            grpc: SocketAddr::new(addr, conf.grpc_listen_port),
+        })
+    }
 }
