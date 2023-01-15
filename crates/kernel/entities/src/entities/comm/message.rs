679165c04ce6ae964f15b4f63e0f37dafcabd3ee
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-15 04:28:17 +0300

    Add `user_id` field to `Message`

--- crates/kernel/entities/src/entities/comm/message.rs
+++ crates/kernel/entities/src/entities/comm/message.rs
@@ -3,9 +3,11 @@ use kernel_proc_macros::entity;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use crate::{entities::link::Instance, traits::*};
-
 use super::{attachment::Attachment, chat::Chat};
+use crate::{
+    entities::{auth::User, link::Instance},
+    traits::*,
+};
 
 #[derive(Clone, Debug, JsonSchema, Serialize, Deserialize)]
 pub enum MessageDirection {
@@ -20,6 +22,7 @@ pub struct Message {
     pub changes: Vec<String>,
     pub attachments: Vec<Attachment>,
     pub direction: MessageDirection,
+    pub user_id: Key<User>,
     pub chat_id: Key<Chat>,
     pub instance_id: Key<Instance>,
     pub delivered_at: DateTime<Utc>,
