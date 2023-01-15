d1a11b01cdc2c209e26d6df827c0d7c3b91e6a1f
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-15 22:26:30 +0300

    Add trait to be implemented by authorization utilities

---
+++ crates/driver/web_common/src/auth/validator.rs
@@ -0,0 +1,29 @@
+use kernel_entities::{
+    entities::auth::{Account, Actions, Resource, User},
+    traits::Key,
+};
+
+pub trait AuthValidator: Sized {
+    type Error;
+
+    fn require<F: FnOnce() -> bool>(
+        &self,
+        req: F,
+    ) -> Result<&Self, Self::Error>;
+
+    fn is_root(&self) -> Result<&Self, Self::Error>;
+
+    fn in_role<'a, R: Into<&'a str>>(
+        &self,
+        role: R,
+    ) -> Result<&Self, Self::Error>;
+
+    fn can<A: Into<Actions> + Copy>(
+        &self,
+        perms: &[(Resource, A)],
+    ) -> Result<&Self, Self::Error>;
+
+    fn is(&self, account_id: &Key<Account>) -> Result<&Self, Self::Error>;
+
+    fn of(&self, user_id: &Key<User>) -> Result<&Self, Self::Error>;
+}
