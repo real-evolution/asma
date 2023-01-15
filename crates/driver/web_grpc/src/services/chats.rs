a110e698f2ee908b8efa2154b0521384303be0da
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-15 04:31:05 +0300

    Reimplement chats grpc service

--- crates/driver/web_grpc/src/services/chats.rs
+++ crates/driver/web_grpc/src/services/chats.rs
@@ -1,22 +1,101 @@
-use tonic::{codegen::BoxStream, Request, Response};
+use std::str::FromStr;
+
+use driver_web_common::state::AppState;
+use futures::StreamExt;
+use kernel_entities::{entities::comm::MessageDirection, traits::Key};
+use kernel_services::{
+    self,
+    comm::chats::{ChatEventKind, ChatsService},
+};
+use tonic::{codegen::BoxStream, Request, Response, Status};
+use tracing::warn;
 
 use crate::proto::{
     models,
-    services::{chats_server::Chats, WatchMessagesRequest},
+    services::{chats_server::Chats, MessageAddedEvent, WatchResponse},
     ProtoResult,
 };
 
-#[derive(Debug)]
-pub(super) struct ChatsService;
+pub(super) struct ChatsServiceImpl {
+    state: AppState,
+}
 
-#[async_trait::async_trait]
-impl Chats for ChatsService {
-    type WatchMessagesStream = BoxStream<models::Message>;
+#[tonic::async_trait]
+impl Chats for ChatsServiceImpl {
+    type WatchStream = BoxStream<WatchResponse>;
 
-    async fn watch_messages(
+    async fn watch(
         &self,
-        _req: Request<WatchMessagesRequest>,
-    ) -> ProtoResult<Response<Self::WatchMessagesStream>> {
-        todo!()
+        req: Request<models::user::Id>,
+    ) -> ProtoResult<Response<Self::WatchStream>> {
+        let Ok(user_id) = Key::from_str(&req.into_inner().value) else {
+            return Err(Status::invalid_argument("invalid key format"));
+        };
+
+        let Ok(mut stream) = self.state.chats.watch_user_chats(&user_id).await else {
+            return Err(Status::invalid_argument("could not subscribe to updates"));
+        };
+
+        let output = async_stream::stream! {
+            while let Some(event) = stream.next().await {
+                let event = match event {
+                    Ok(event) => event,
+                    Err(err) => {
+                        warn!("an error occured while reading event: {err:#?}");
+
+                        yield Err(Status::internal("could not read event"));
+
+                        return ();
+                    }
+                };
+
+                match event.kind {
+                    | ChatEventKind::MessageAdded {
+                        id,
+                        text,
+                        instance_id,
+                        direction,
+                        created_at,
+                    } => {
+                        let direction: models::message::Direction =
+                            direction.into();
+
+                        yield Ok(WatchResponse {
+                            message_added: Some(MessageAddedEvent {
+                                id: Some(models::message::Id {
+                                    value: id.to_string(),
+                                }),
+                                chat_id: Some(models::chat::Id {
+                                    value: event.chat_id.to_string(),
+                                }),
+                                instance_id: Some(models::instance::Id {
+                                    value: instance_id.to_string(),
+                                }),
+                                direction: direction.into(),
+                                text: text.unwrap_or_default(),
+                                created_at: Some(prost_types::Timestamp {
+                                    seconds: created_at.timestamp(),
+                                    nanos: created_at.timestamp_nanos() as i32,
+                                }),
+                            }),
+                        });
+                    }
+                }
+            }
+
+            return ()
+        }
+        .boxed();
+
+        Ok(Response::new(output))
+    }
+}
+
+impl From<MessageDirection> for models::message::Direction {
+    fn from(value: MessageDirection) -> Self {
+        match value {
+            | MessageDirection::Incoming => Self::Incoming,
+            | MessageDirection::Outgoing => Self::Outgoing,
+        }
     }
 }
