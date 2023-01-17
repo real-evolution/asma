874f0325b253ad8b4c26c94644c1bc3a9e46b77a
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-17 21:46:28 +0300

    Implement `CommDataStore`

--- crates/adapter/repositories/postgres/src/comm/mod.rs
+++ crates/adapter/repositories/postgres/src/comm/mod.rs
@@ -1,1 +1,23 @@
 mod bots;
+
+use kernel_repositories::comm::{BotsRepo, CommDataStore};
+
+use crate::database::SqlxPool;
+
+pub(crate) struct SqlxCommDataStore {
+    bots: bots::SqlxBotsRepo,
+}
+
+impl SqlxCommDataStore {
+    pub(crate) fn new(pool: SqlxPool) -> Self {
+        Self {
+            bots: bots::SqlxBotsRepo(pool),
+        }
+    }
+}
+
+impl CommDataStore for SqlxCommDataStore {
+    fn bots(&self) -> &dyn BotsRepo {
+        &self.bots
+    }
+}
