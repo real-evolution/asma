3bba06a647727497357eb3f55d58f779414ca3da
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-17 22:24:32 +0300

    Implement bot creation endpoint

---
+++ crates/driver/web_rest/src/api/comm/bots/add.rs
@@ -0,0 +1,33 @@
+use axum::extract::State;
+use driver_web_common::{auth::validator::AuthValidator, state::AppState};
+use kernel_entities::entities::{auth::*, comm::Bot};
+use kernel_repositories::comm::InsertBot;
+
+use super::dtos::AddBotDto;
+use crate::{
+    error::ApiResult,
+    extractors::validated_json::ValidatedJson,
+    util::{
+        auth::token::RestAuthToken,
+        response::{Created, EntityCreated},
+    },
+};
+
+pub async fn add(
+    auth: RestAuthToken,
+    state: State<AppState>,
+    ValidatedJson(form): ValidatedJson<AddBotDto>,
+) -> ApiResult<EntityCreated<Bot>> {
+    auth.can(&[(Resource::Roles, Action::Add)])?
+        .of(&form.user_id)
+        .or_else(|_| auth.in_role(KnownRoles::Admin))?;
+
+    let bot = state
+        .data
+        .comm()
+        .bots()
+        .create(InsertBot::new(form.name, form.is_active, form.user_id))
+        .await?;
+
+    Ok(Created::new("/api/auth/roles", bot).into())
+}
