9e94561daf5d13664427472d93314b73fcc74ca6
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-17 21:45:58 +0300

    Add `CommDataStore` to `DataStore` trait

--- crates/kernel/repositories/src/lib.rs
+++ crates/kernel/repositories/src/lib.rs
@@ -1,5 +1,4 @@
 mod database_connection;
-pub mod traits;
 
 pub use database_connection::*;
 
@@ -7,11 +6,13 @@ pub mod auth;
 pub mod comm;
 pub mod error;
 pub mod link;
+pub mod traits;
 
 pub trait DataStore: Send + Sync {
     fn tx(&self) -> &dyn TransactionManager;
     fn auth(&self) -> &dyn auth::AuthDataStore;
     fn link(&self) -> &dyn link::LinkDataStore;
+    fn comm(&self) -> &dyn comm::CommDataStore;
 }
 
 pub trait DocumentStore: Send + Sync {
