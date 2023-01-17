13f32338e4bdacef1943c0f4b69e4ab8345e2851
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-17 22:12:40 +0300

    Add a router for bots endpoints

--- crates/driver/web_rest/src/api/comm/mod.rs
+++ crates/driver/web_rest/src/api/comm/mod.rs
@@ -1,1 +1,8 @@
-pub mod bots;
+mod bots;
+
+use aide::axum::ApiRouter;
+use driver_web_common::state::AppState;
+
+pub fn routes() -> ApiRouter<AppState> {
+    ApiRouter::new().nest("/bots", bots::routes())
+}
