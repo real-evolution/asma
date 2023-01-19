df29b00fe02a39d97cae1faa473d90287a1f7562
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-19 22:35:52 +0300

    Add authorized extension traits for `InsertRepo` and `ChildRepo` traits

--- crates/driver/web_common/src/auth/util/authed_repo.rs
+++ crates/driver/web_common/src/auth/util/authed_repo.rs
@@ -1,15 +1,18 @@
 use chrono::{DateTime, Utc};
 use kernel_entities::{
-    entities::auth::Action,
+    entities::auth::{Action, KnownRoles, User},
     traits::{Entity, Key},
     util::ResourceEntity,
 };
-use kernel_repositories::{error::RepoError, traits::Repo};
+use kernel_repositories::{
+    error::RepoError,
+    traits::{ChildRepo, InsertRepo, Repo},
+};
 
 use crate::auth::validator::AuthValidator;
 
 #[async_trait::async_trait]
-pub trait AuthedRepoExts<E: Entity>: Repo<Entity = E> {
+pub trait RepoExt<E: Entity>: Repo<Entity = E> {
     async fn get_authed<V, VE>(
         &self,
         key: &Key<Self::Entity>,
@@ -49,7 +52,60 @@ pub trait AuthedRepoExts<E: Entity>: Repo<Entity = E> {
 }
 
 #[async_trait::async_trait]
-impl<E, R> AuthedRepoExts<E> for R
+pub trait InsertRepoExt<'a, E, I>: InsertRepo<I, Entity = E>
+where
+    I: 'a,
+{
+    async fn create_authed<V, VE>(
+        &'a self,
+        model: I,
+        auth: &V,
+    ) -> Result<Self::Entity, VE>
+    where
+        V: AuthValidator<Error = VE>,
+        VE: From<RepoError>;
+}
+
+#[async_trait::async_trait]
+pub trait ChildRepoExt<P, E>: ChildRepo<P, Entity = E>
+where
+    P: Entity,
+    E: Entity,
+{
+    async fn get_paginated_of_authed<const ALLOW_ADMIN: bool, V, VE>(
+        &self,
+        parent_key: &Key<P>,
+        before: &DateTime<Utc>,
+        limit: usize,
+        auth: &V,
+    ) -> Result<Vec<Self::Entity>, VE>
+    where
+        V: AuthValidator<Error = VE>,
+        VE: From<RepoError>;
+
+    async fn get_of_authed<const ALLOW_ADMIN: bool, V, VE>(
+        &self,
+        parent_key: &Key<P>,
+        key: &Key<Self::Entity>,
+        auth: &V,
+    ) -> Result<Self::Entity, VE>
+    where
+        V: AuthValidator<Error = VE>,
+        VE: From<RepoError>;
+
+    async fn remove_of_authed<const ALLOW_ADMIN: bool, V, VE>(
+        &self,
+        parent_key: &Key<P>,
+        key: &Key<Self::Entity>,
+        auth: &V,
+    ) -> Result<(), VE>
+    where
+        V: AuthValidator<Error = VE>,
+        VE: From<RepoError>;
+}
+
+#[async_trait::async_trait]
+impl<E, R> RepoExt<E> for R
 where
     R: Repo<Entity = E> + ?Sized,
     E: ResourceEntity,
@@ -109,5 +165,105 @@ where
         auth.can(&[(E::resource(), Action::Remove)])?;
 
         self.remove(key).await.map_err(From::from)
+    }
+}
+
+#[async_trait::async_trait]
+impl<'a, E, I, R> InsertRepoExt<'a, E, I> for R
+where
+    R: InsertRepo<I, Entity = E> + ?Sized,
+    E: ResourceEntity,
+    I: Send + 'a,
+{
+    async fn create_authed<V, VE>(
+        &'a self,
+        model: I,
+        auth: &V,
+    ) -> Result<Self::Entity, VE>
+    where
+        V: AuthValidator<Error = VE>,
+        VE: From<RepoError>,
+    {
+        auth.can(&[(E::resource(), Action::Add)])?;
+
+        self.create(model).await.map_err(From::from)
+    }
+}
+
+#[async_trait::async_trait]
+impl<E: ResourceEntity, R> ChildRepoExt<User, E> for R
+where
+    R: ChildRepo<User, Entity = E>,
+{
+    async fn get_paginated_of_authed<const ALLOW_ADMIN: bool, V, VE>(
+        &self,
+        parent_key: &Key<User>,
+        before: &DateTime<Utc>,
+        limit: usize,
+        auth: &V,
+    ) -> Result<Vec<Self::Entity>, VE>
+    where
+        V: AuthValidator<Error = VE>,
+        VE: From<RepoError>,
+    {
+        auth.can(&[(E::resource(), Action::View)])?
+            .of(parent_key)
+            .or_else(|err| {
+                if ALLOW_ADMIN {
+                    auth.in_role(KnownRoles::Admin)
+                } else {
+                    Err(err)
+                }
+            })?;
+
+        self.get_paginated_of(parent_key, before, limit)
+            .await
+            .map_err(From::from)
+    }
+
+    async fn get_of_authed<const ALLOW_ADMIN: bool, V, VE>(
+        &self,
+        parent_key: &Key<User>,
+        key: &Key<Self::Entity>,
+        auth: &V,
+    ) -> Result<Self::Entity, VE>
+    where
+        V: AuthValidator<Error = VE>,
+        VE: From<RepoError>,
+    {
+        auth.can(&[(E::resource(), Action::View)])?
+            .of(parent_key)
+            .or_else(|err| {
+                if ALLOW_ADMIN {
+                    auth.in_role(KnownRoles::Admin)
+                } else {
+                    Err(err)
+                }
+            })?;
+
+        self.get_of(parent_key, key).await.map_err(From::from)
+    }
+
+    async fn remove_of_authed<const ALLOW_ADMIN: bool, V, VE>(
+        &self,
+        parent_key: &Key<User>,
+        key: &Key<Self::Entity>,
+        auth: &V,
+    ) -> Result<(), VE>
+    where
+        V: AuthValidator<Error = VE>,
+        VE: From<RepoError>,
+    {
+        auth.can(&[(E::resource(), Action::Remove)])?
+            .of(parent_key)
+            .or_else(|err| {
+                if ALLOW_ADMIN {
+                    auth.in_role(KnownRoles::Admin)
+                } else {
+                    Err(err)
+                }
+            })?;
+
+        self.remove_of(parent_key, key).await.map_err(From::from)
     }
 }
