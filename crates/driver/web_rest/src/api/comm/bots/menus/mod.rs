dd03d784a45e67a0d3dc3c3d6c108ccddca98bb8
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-20 00:02:49 +0300

    Add menus querying endpoints

--- crates/driver/web_rest/src/api/comm/bots/menus/mod.rs
+++ crates/driver/web_rest/src/api/comm/bots/menus/mod.rs
@@ -1,7 +1,11 @@
 mod dtos;
-use aide::axum::ApiRouter;
+mod view;
+
+use aide::axum::{routing::get, ApiRouter};
 use driver_web_common::state::AppState;
 
 pub fn routes() -> ApiRouter<AppState> {
     ApiRouter::new()
+        .api_route("/", get(view::get_all))
+        .api_route("/:menu_id", get(view::get_by_id))
 }
---
+++ crates/driver/web_rest/src/api/comm/bots/menus/view.rs
@@ -0,0 +1,60 @@
+use axum::{
+    extract::{Path, State},
+    Json,
+};
+use driver_web_common::{auth::validator::AuthValidator, state::AppState};
+use itertools::Itertools;
+use kernel_entities::{
+    entities::{
+        auth::*,
+        comm::{Bot, Menu},
+    },
+    traits::Key,
+};
+
+use super::dtos::MenuDto;
+use crate::{
+    api::dtos::pagination::Pagination,
+    error::ApiResult,
+    util::auth::token::RestAuthToken,
+};
+
+pub async fn get_all(
+    auth: RestAuthToken,
+    state: State<AppState>,
+    bot_id: Path<Key<Bot>>,
+    pagination: Pagination,
+) -> ApiResult<Json<Vec<MenuDto>>> {
+    auth.can(&[(Resource::Menu, Action::View)])?;
+
+    let menus = state
+        .data
+        .comm()
+        .menus()
+        .get_paginated_of(&bot_id, &pagination.before, pagination.page_size)
+        .await?
+        .into_iter()
+        .map(MenuDto::from)
+        .collect_vec();
+
+    Ok(Json(menus))
+}
+
+pub async fn get_by_id(
+    auth: RestAuthToken,
+    bot_id: Path<Key<Bot>>,
+    menu_id: Path<Key<Menu>>,
+    state: State<AppState>,
+) -> ApiResult<Json<MenuDto>> {
+    auth.can(&[(Resource::Menu, Action::View)])?;
+
+    Ok(Json(
+        state
+            .data
+            .comm()
+            .menus()
+            .get_of(&bot_id, &menu_id)
+            .await?
+            .into(),
+    ))
+}
