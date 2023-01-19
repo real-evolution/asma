8a1da780e678a40cd00a6c5d661d3fc8e9f5fef2
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-20 00:17:34 +0300

    Implement menus creation endpoint

---
+++ crates/driver/web_rest/src/api/comm/bots/menus/add.rs
@@ -0,0 +1,44 @@
+use axum::extract::State;
+use driver_web_common::{auth::validator::AuthValidator, state::AppState};
+use kernel_entities::entities::{auth::*, comm::Menu};
+use kernel_repositories::comm::InsertMenu;
+
+use super::dtos::AddMenuDto;
+use crate::{
+    error::ApiResult,
+    extractors::validated_json::ValidatedJson,
+    util::{
+        auth::token::RestAuthToken,
+        response::{Created, EntityCreated},
+    },
+};
+
+pub async fn add(
+    auth: RestAuthToken,
+    state: State<AppState>,
+    ValidatedJson(form): ValidatedJson<AddMenuDto>,
+) -> ApiResult<EntityCreated<Menu>> {
+    auth.can(&[(Resource::Menu, Action::Add)])?;
+
+    let menu_bot = state.data.comm().bots().get(&form.bot_id).await?;
+
+    auth.of(&menu_bot.user_id)
+        .or_else(|_| auth.in_role(KnownRoles::Admin))?;
+
+    let menu = state
+        .data
+        .comm()
+        .menus()
+        .create(InsertMenu::new(
+            form.title,
+            form.content,
+            form.menu_trigger,
+            form.matching_strategy,
+            form.is_active,
+            form.parent_menu_id,
+            form.bot_id,
+        ))
+        .await?;
+
+    Ok(Created::new("/api/comm/bots/menus", menu).into())
+}
--- crates/driver/web_rest/src/api/comm/bots/menus/mod.rs
+++ crates/driver/web_rest/src/api/comm/bots/menus/mod.rs
@@ -1,3 +1,4 @@
+mod add;
 mod dtos;
 mod remove;
 mod view;
@@ -7,6 +8,6 @@ use driver_web_common::state::AppState;
 
 pub fn routes() -> ApiRouter<AppState> {
     ApiRouter::new()
-        .api_route("/", get(view::get_all))
+        .api_route("/", get(view::get_all).post(add::add))
         .api_route("/:menu_id", get(view::get_by_id).delete(remove::remove))
 }
