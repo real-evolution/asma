98a98e7def7302799371da1cd0aec149e29ee591
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-20 05:16:23 +0300

    Add a method to stream all active bots

--- crates/kernel/repositories/src/comm/bots.rs
+++ crates/kernel/repositories/src/comm/bots.rs
@@ -1,15 +1,17 @@
 use derive_more::Constructor;
+use futures::stream::BoxStream;
 use kernel_entities::{
     entities::{auth::User, comm::Bot},
     traits::Key,
 };
 
-use crate::traits::*;
+use crate::{error::RepoResult, traits::*};
 
 #[async_trait::async_trait]
 pub trait BotsRepo:
     Repo<Entity = Bot> + InsertRepo<InsertBot> + ChildRepo<User> + Send + Sync
 {
+    fn stream_active(&self) -> BoxStream<'_, RepoResult<Bot>>;
 }
 
 #[derive(Constructor)]
