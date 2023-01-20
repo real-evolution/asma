1db17c9b8ca12580541477362297f79c8152afe1
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-20 05:18:46 +0300

    Pass instance id and message content instead of the whole message

--- crates/app/services/src/comm/bots/bot_context.rs
+++ crates/app/services/src/comm/bots/bot_context.rs
@@ -1,10 +1,7 @@
 use std::{collections::HashMap, sync::Arc};
 
 use kernel_entities::{
-    entities::{
-        comm::{Menu, Message},
-        link::Instance,
-    },
+    entities::{comm::Menu, link::Instance},
     traits::Key,
 };
 use kernel_repositories::DataStore;
@@ -15,32 +12,37 @@ use super::menu_traverser::MenuTraverser;
 
 pub(super) struct BotContext {
     data: Arc<dyn DataStore>,
-    root: Menu,
+    entry: Menu,
     active: RwLock<HashMap<Key<Instance>, MenuTraverser>>,
 }
 
 impl BotContext {
+    pub(super) fn new(entry: Menu, data: Arc<dyn DataStore>) -> Self {
+        Self {
+            data,
+            entry,
+            active: Default::default(),
+        }
+    }
+
     pub(super) async fn handle_message(
         &self,
-        msg: &Message,
+        instance_id: &Key<Instance>,
+        text: &str,
     ) -> AppResult<Option<String>> {
-        let Some(ref text) = msg.text else {
-            return Ok(None);
-        };
-
         let active = self.active.read().await;
 
-        if !active.contains_key(&msg.instance_id) {
+        if !active.contains_key(instance_id) {
             let traverser =
-                MenuTraverser::new(&self.root.id, self.data.clone()).await?;
+                MenuTraverser::new(&self.entry.id, self.data.clone()).await?;
 
             self.active
                 .write()
                 .await
-                .insert(msg.instance_id.clone(), traverser);
+                .insert(instance_id.clone(), traverser);
         }
 
-        if let Some(traverser) = active.get(&msg.instance_id) {
+        if let Some(traverser) = active.get(instance_id) {
             if traverser.process(text).await? {
                 return Ok(Some(traverser.to_formatted_string().await));
             }
