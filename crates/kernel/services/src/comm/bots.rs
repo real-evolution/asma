a734f49380411c700079456221777d5862008887
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-20 05:15:42 +0300

    Remove `start_all` and `stop_all` methods from `BotsService`

--- crates/kernel/services/src/comm/bots.rs
+++ crates/kernel/services/src/comm/bots.rs
@@ -1,8 +1,2 @@
-use futures::stream::BoxStream;
-
-use crate::error::AppResult;
-
-pub trait BotsService {
-    fn start_all(&self) -> BoxStream<'_, AppResult<()>>;
-    fn stop_all(&self) -> BoxStream<'_, AppResult<()>>;
-}
+#[async_trait::async_trait]
+pub trait BotsService: Send + Sync {}
