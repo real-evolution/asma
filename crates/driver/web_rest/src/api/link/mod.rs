77ba83a80e53d6937b420941b1acecd00b6da545
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-01 22:51:06 +0300

    Add instances router

--- crates/driver/web_rest/src/api/link/mod.rs
+++ crates/driver/web_rest/src/api/link/mod.rs
@@ -1,8 +1,11 @@
 mod channels;
+mod instances;
 
 use aide::axum::ApiRouter;
 use driver_web_common::state::AppState;
 
 pub fn routes() -> ApiRouter<AppState> {
-    ApiRouter::new().nest("/channels", channels::routes())
+    ApiRouter::new()
+        .nest("/channels", channels::routes())
+        .nest("/instances", instances::routes())
 }
