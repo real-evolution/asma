563806e9896148af05d8fcac99fc06bab1ad9cf1
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-02 22:12:21 +0300

    Register `GrpcStatsService` in the gRPC router

--- crates/driver/web_grpc/src/app.rs
+++ crates/driver/web_grpc/src/app.rs
@@ -2,11 +2,12 @@ use driver_web_common::state::AppState;
 use tonic::transport::{server::Router, Server};
 
 use crate::{
-    proto::services::chats_server::ChatsServer,
-    services::GrpcChatsService,
+    proto::services::{chats_server::ChatsServer, stats_server::StatsServer},
+    services::{GrpcChatsService, GrpcStatsService},
 };
 
 pub fn make_grpc_app(state: AppState) -> Router {
     Server::builder()
         .add_service(ChatsServer::new(GrpcChatsService::new(state.clone())))
+        .add_service(StatsServer::new(GrpcStatsService::new(state)))
 }
